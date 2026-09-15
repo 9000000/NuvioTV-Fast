d1b7e5268fd1d7a24f3aea021b3151501cadd5cc
9000000 <hai90000001@gmail.com>
2026-09-15 08:24:59 +0700

    upver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,8 +115,8 @@ android {
         applicationId = "com.nuvio.tv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1065
-        versionName = "0.9.10-beta"
+        versionCode = 1066
+        versionName = "0.9.11-beta"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
