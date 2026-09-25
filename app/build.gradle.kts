3f899d26dd13821041068b775d079d4b4a3ac53e
9000000 <hai90000001@gmail.com>
2026-09-25 13:59:19 +0700

    feat: add Android app module build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,8 +116,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1093
-        versionName = "1.1.6"
+        versionCode = 1094
+        versionName = "1.1.7"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
